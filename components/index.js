c80efe5c31782d23ba471d9da574797e4297a89b
Manav Misra <manavm@visionwebsoft.com>
2021-02-09 14:41:02 -0600

    :sparkles: Render first functional component

---
+++ components/index.js
@@ -0,0 +1,2 @@
+// TODO: Is import the default from 'test' and export out as 'testComponent'
+export { default as testComponent } from "./test";
--- scripts.js
+++
@@ -1,7 +0,0 @@
-import { catalog } from "./lib/index.js";
-
-console.log(catalog);
-
-// TODO: Dynamically render the data into a table using the 'manual HTML' as a guide.
-
-// TODO: Apply filtering to show hide/re-render the table accordingly.
